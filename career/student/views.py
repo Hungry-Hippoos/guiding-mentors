94b0834e74e77aea0bb7556479954c680584a8d6
manan <mrpandya2017@gmail.com>
2022-04-09 14:00:59 +0530

    views converted into class

--- career/student/views.py
+++ career/student/views.py
@@ -4,17 +4,18 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 
 # Create your views here.
-def loginPage(request):
-
-    if request.method=='POST':
-       username=request.POST.get('username')
-       password=request.POST.get('password')
-       user=authenticate(request,username=username,password=password)
-       if user is not None:
-           login(request,user)
-           return redirect('home1')
-       else :
-           messages.info(request,'Username or password incorrect')
-    context={}
-    return render(request,'myApp/login.html',context)
-
+class loginPage:
+    
+    def post(self, request):
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home1')
+        else :
+            messages.info(request,'Username or password incorrect')
+    
+    def get(self, request):
+        context={}
+        return render(request,'myApp/login.html',context)
