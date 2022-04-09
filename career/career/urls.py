14114011df46112d5e1c7382d97e47178e90d177
smeet07 <smeetbn@gmail.com>
2022-04-09 19:24:42 +0530

    quiz kiya hai

--- career/career/urls.py
+++ career/career/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from student.views import loginPage,registerPage
+from student.views import loginPage,registerPage,homepage,quizPage
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -26,5 +26,7 @@ urlpatterns = [
     path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(template_name="password_reset_sent.html"),name="password_reset_done"),
     path('reset/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(template_name="reset_form.html"), name="password_reset_confirm"),
     path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_done.html"),name="password_reset_complete"),
+    path('home/',homepage,name='home'),
+    path('quiz/',quizPage,name='quiz'),
 
 ]
