ce6319f051733e84635729ac723d671048ddf153
manan <mrpandya2017@gmail.com>
2022-04-10 04:40:58 +0530

    lmao

--- career/apti/views.py
+++ career/apti/views.py
@@ -3,6 +3,7 @@ from apti.services.calc_weights import get_weights, generate_field, normalize_ra
 from student.models import StudentBuffer
 from apti.services.get_student_details import getStudentBuffer
 from django.views.decorators.csrf import csrf_exempt
+from .services.skills import skills
 import pandas as pd
 import plotly
 import plotly.express as px
@@ -25,8 +26,28 @@ def student_dashboard(request,responses=None):
 def quizPage(request,student_id=None):
     if request.method == 'POST':
         print(request.POST)
+        data = request.POST
         print(student_id)
-        return redirect('login')
+        if student_id is None:
+            return redirect('login')
+        student = getStudentBuffer(student_id)
+        student.q1=",".join(data['0'])
+        student.q2=",".join(data['1'])
+        student.q3=",".join(data['2'])
+        student.q4=",".join(data['3'])
+        student.q5=",".join(data['4'])
+        student.q6=",".join(data['5'])
+        student.q7=",".join(data['6'])
+        ski=[]
+        for i in data['4']:
+            ski+=skills[i]
+        student.skills=",".join(ski)
+        student.recommended = generate_field(data)
+        try:
+            student.save()
+            return redirect('student_dashboard')
+        except:
+            return redirect('login')
     return render(request,'quiz.html')
 
 
