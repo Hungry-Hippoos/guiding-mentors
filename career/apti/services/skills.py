04834d9b3585258ae563332ab4a786a0c67725a5
Hinal Pujara <hinal.pujara@somaiya.edu>
2022-04-09 20:04:26 +0530

    skills

---
+++ career/apti/services/skills.py
@@ -0,0 +1,16 @@
+skills = {
+    "software development": ["Time Management", "problem solving", "Technology skills"],
+    "supply chain manager": ["Understanding of global issues", "Business ethics", "Project management"],
+    "scientist": ["problem solving", "predicting", "research skills"],
+    "pilot": ["confidence", "adaptability", "multitasking"],
+    "chef": ["willingness to learn", "time management", "creativity"],
+    "author": ["discipline", "persistent", "adaptability"],
+    "Journalist": ["open minded", "story telling", "research skills"],
+    "Astronaut": ["Observational skills", "Resilience", "Teamwork"],
+    "Risk analyst": ["good research skills", "planning skills", "negotiation skills"],
+    "Psychiatrist": ["Compassion and Empathy", "Stress Tolerance", "Perception and Sensitivity"],
+    "CA": ["Confidence", "Analytical and Logical Ability", "Persistence"],
+    "Doctor": ["optimistic", "Leadership Skills", "Responsibility and Reliability"],
+    "Sales": ["Positive Thinking", "Interpersonal Skills", "Handling rejection"],
+    "teacher": ["teaching ability","patience","understanding","communication skills"]
+}
